2b52711f6a63bc3d773a5b619a9999c84afa6d7e
HexagonalED <shuin3180@naver.com>
2015-10-01 23:40:43 +0900

    PL_Delay1day_opt2

--- pl/hw2/exercise7.ml
+++ pl/hw2/exercise7.ml
@@ -1,20 +1,41 @@
-signature ZEXPR =
-  sig
+module type ZEXPR = 
+  sig 
+    exception Error of string 
+    type id = string 
+    type expr = 
+      | NUM of int 
+      | PLUS of expr * expr 
+      | MINUS of expr * expr 
+      | MULT of expr * expr 
+      | DIVIDE of expr * expr 
+      | MAX of expr list 
+      | VAR of id 
+      | LET of id * expr * expr 
+      
+    type environment 
+    type value 
+    
+    val emptyEnv : environment 
+    val eval : environment * expr -> value 
+    
+    val print_value : value -> unit 
+  end 
+  
+module Zexpr : ZEXPR =
+  struct
     exception Error of string
     type id = string
     type expr = NUM of int
     | PLUS of expr * expr
     | MINUS of expr * expr
-    6
     | MULT of expr * expr
     | DIVIDE of expr * expr
     | MAX of expr list
     | VAR of id
     | LET of id * expr * expr
-    type environment
-    type value
-    val emptyEnv: environment
+
+    type environment = list
+    type value = 
+    val emptyEnv: environment = []
     val eval: env * expr -> value
   end
-
-
