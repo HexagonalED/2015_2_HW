d5b8f12190c447a0321200e5afcdbd2b5068183e
HexagonalED <shuin3180@naver.com>
2015-10-01 23:00:11 +0900

    PL2_Delay1day

--- pl/hw2/exercise4.ml
+++ pl/hw2/exercise4.ml
@@ -1,38 +1,36 @@
-type item = string;;
+type item = string
 type tree = LEAF of item
-| NODE of tree list;;
-exception NOMOVE of string;;
+| NODE of tree list
 type zipper = TOP
-| HAND of tree list * zipper * tree list;;
-type location = LOC of tree * zipper;;
+| HAND of tree list * zipper * tree list
+type location = LOC of tree * zipper
+exception NOMOVE of string
 
 let goLeft loc =
   match loc with
   LOC(t, TOP) -> raise (NOMOVE "left of top")
   | LOC(t, HAND(l::left, up, right)) -> LOC(l, HAND(left, up, t::right))
-  | LOC(t, HAND([],up,right)) -> raise NOMOVE "left of first"
+  | LOC(t, HAND([],up,right)) -> raise (NOMOVE "left of first")
 ;;
 
 let goRight : location -> location = fun loc -> 
   match loc with
   LOC(t, TOP) -> raise (NOMOVE "right of top")
   | LOC(t, HAND(left, up, r::right)) -> LOC(r, HAND(t::left, up, right))
-  | LOC(t, HAND(left,up,[])) -> raise NOMOVE "right of first"
+  | LOC(t, HAND(left,up,[])) -> raise (NOMOVE "right of first")
 ;;
-
 
 let goUp : location -> location = fun loc -> 
   match loc with
-
-  | LOC(t, HAND(left, HAND([],z,r::rt), right)) -> LOC(r, HAND(left@(t::right),z,rt))
+  | LOC(t, TOP) -> raise (NOMOVE "At The Top")
+  | LOC(t, HAND(left, up, right)) -> LOC(NODE((List.rev left)@(t::right)),up)
 ;;
 
 
-let goDown : location -> location = fun loc -> 
+let goDown : location -> location = fun loc ->
   match loc with
-  LOC(t, h)raise (NOMOVE "left of top")
-  | LOC(t, HAND(l::left, up, right)) -> LOC(l, HAND(left, up, t::right))
-  | LOC(t, HAND([],up,right)) -> raise NOMOVE "left of first"
+  LOC(LEAF(l), zip) -> raise (NOMOVE "At the Leaf")
+  | LOC(NODE(hd::tl), zip) -> LOC(hd, HAND([],zip,tl))
+  | LOC(NODE([]), zip) -> raise (NOMOVE "NODE[]ERROR")
 ;;
-
 
